190dfdd29cdce32b6a10e3b448172b91e05d6aeb
DESKTOP-U1GLCMN\NguyenMinhThuong <thuonghtml@gmail.com>
2017-10-11 21:30:35 +0700

    Input validation and showing Validation Error

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,16 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('create', function () {
         return view('admin.create');
     })->name('admin.create');
-    Route::post('create', function(\Illuminate\Http\Request $request) {
-        return "It works!";
+    Route::post('create', function(\Illuminate\Http\Request $request, \Illuminate\Validation\Factory $validator) {
+        $validation = $validator->make($request->all(),[
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+        if($validation->fails())
+            return redirect()->back()->withErrors($validation);
+      return redirect()
+        ->route('admin.index')
+        ->with('info','Post created, Title ' . $request->input('title'));
     })->name('admin.create');
     Route::get('edit/{id}', function ($id) {
          if($id == 1)
@@ -49,8 +57,14 @@ Route::group(['prefix' => 'admin'], function() {
 			}
         return view('admin.edit', ['post' => $post]);
     })->name('admin.edit');
-    Route::post('edit', function(\Illuminate\Http\Request $request) {
-        return redirect()
+     Route::post('edit', function(\Illuminate\Http\Request $request, \Illuminate\Validation\Factory $validator) {
+        $validation = $validator->make($request->all(),[
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+        if($validation->fails())
+            return redirect()->back()->withErrors($validation);
+      return redirect()
         ->route('admin.index')
         ->with('info','Post editted, new Title ' . $request->input('title'));
     })->name('admin.update');

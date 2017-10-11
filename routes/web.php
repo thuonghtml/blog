3f7d78a2761abfe5f648059427ab117e2f6b1174
DESKTOP-U1GLCMN\NguyenMinhThuong <thuonghtml@gmail.com>
2017-10-11 21:08:58 +0700

    Route group

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,49 @@
 Route::get('/', function () {
      return view('blog.index');
 })->name('blog.index');
+
 Route::get('post/{id}', function () {
     return view('blog.post');
 })->name('blog.post');
+
 Route::get('about', function () {
     return view('other.about');
 })->name('other.about');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('', function () {
+        return view('admin.index');
+    })->name('admin.index');
+    Route::get('create', function () {
+        return view('admin.create');
+    })->name('admin.create');
+    Route::post('create', function() {
+        return "It works!";
+    })->name('admin.create');
+    Route::get('edit/{id}', function () {
+        return view('admin.edit');
+    })->name('admin.edit');
+    Route::post('edit', function() {
+        return "It works!";
+    })->name('admin.update');
+});
+/*
 Route::get('admin/create', function () {
     return view('admin.create');
 })->name('admin.create');
+
 Route::post('admin/create', function(){
 	return "It works!";
 })->name('admin.create');
+
 Route::get('admin', function () {
     return view('admin.index');
 })->name('admin.index');
+
 Route::get('admin/edit/{id}', function () {
     return view('admin.edit');
 })->name('admin.edit');
+
 Route::post('admin/update', function () {
     return view('admin.update');
-})->name('admin.update');
+})->name('admin.update');*/

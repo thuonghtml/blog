05701cc25feefbadb582dfb39c7d752eacf2f8f3
DESKTOP-U1GLCMN\NguyenMinhThuong <thuonghtml@gmail.com>
2017-10-11 20:55:05 +0700

    Create and using Post Route

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,15 @@ Route::get('about', function () {
 Route::get('admin/create', function () {
     return view('admin.create');
 })->name('admin.create');
+Route::post('admin.create',function(){
+	return "It work!";
+})->name('admin.create');
 Route::get('admin', function () {
     return view('admin.index');
 })->name('admin.index');
 Route::get('admin/edit/{id}', function () {
     return view('admin.edit');
 })->name('admin.edit');
-Route::get('admin/update', function () {
+Route::post('admin/update', function () {
     return view('admin.update');
 })->name('admin.update');

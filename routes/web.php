bb57ad478ed899781544ec992a2677252f86181a
DESKTOP-U1GLCMN\NguyenMinhThuong <thuonghtml@gmail.com>
2017-10-12 10:08:55 +0700

    Create Post Model, PostController, Connecting Controller Action & Route

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,29 @@
 <?php
 
-Route::get('/', function () {
-     return view('blog.index');
-})->name('blog.index');
-
+Route::get('/', 'PostController@getIndex')->name('blog.index');
 Route::get('post/{id}', function ($id) {
     if($id == 1)
-	{
-		$post = [
-				'title' => 'Learning Laravel',
-				'content' => 'This blog post will get you right on track with Laravel'
-		];
-	}else{
-		$post = [
-						'title' => 'Something else',
-						'content' => 'Some other content'
-				];
-	}
+    {
+        $post = [
+                'title' => 'Learning Laravel',
+                'content' => 'This blog post will get you right on track with Laravel'
+        ];
+    }else{
+        $post = [
+                        'title' => 'Something else',
+                        'content' => 'Some other content'
+                ];
+    }
     return view('blog.post', ['post' => $post]);
 })->name('blog.post');
-
 Route::get('about', function () {
     return view('other.about');
 })->name('other.about');
-
 Route::group(['prefix' => 'admin'], function() {
     Route::get('', function () {
         return view('admin.index');
     })->name('admin.index');
-    Route::get('create', function () {
+    Route::get('create', function () { 
         return view('admin.create');
     })->name('admin.create');
     Route::post('create', function(\Illuminate\Http\Request $request, \Illuminate\Validation\Factory $validator) {
@@ -43,21 +38,21 @@ Route::group(['prefix' => 'admin'], function() {
         ->with('info','Post created, Title ' . $request->input('title'));
     })->name('admin.create');
     Route::get('edit/{id}', function ($id) {
-         if($id == 1)
-			{
-				$post = [
-						'title' => 'Learning Laravel',
-						'content' => 'This blog post will get you right on track with Laravel'
-				];
-			}else{
-				$post = [
-								'title' => 'Something else',
-								'content' => 'Some other content'
-						];
-			}
+           if($id == 1)
+            {
+                $post = [
+                        'title' => 'Learning Laravel',
+                        'content' => 'This blog post will get you right on track with Laravel'
+                ];
+            }else{
+                $post = [
+                                'title' => 'Something else',
+                                'content' => 'Some other content'
+                        ];
+            }
         return view('admin.edit', ['post' => $post]);
     })->name('admin.edit');
-     Route::post('edit', function(\Illuminate\Http\Request $request, \Illuminate\Validation\Factory $validator) {
+    Route::post('edit', function(\Illuminate\Http\Request $request, \Illuminate\Validation\Factory $validator) {
         $validation = $validator->make($request->all(),[
             'title' => 'required|min:5',
             'content' => 'required|min:10'
@@ -70,22 +65,96 @@ Route::group(['prefix' => 'admin'], function() {
     })->name('admin.update');
 });
 /*
+Route::get('/', function () {
+    return view('blog.index');
+})->name('blog.index');
+Route::get('post/{id}', function ($id) {
+    if($id == 1)
+    {
+        $post = [
+                'title' => 'Learning Laravel',
+                'content' => 'This blog post will get you right on track with Laravel'
+        ];
+    }else{
+        $post = [
+                        'title' => 'Something else',
+                        'content' => 'Some other content'
+                ];
+    }
+    return view('blog.post', ['post' => $post]);
+})->name('blog.post');
+Route::get('about', function () {
+    return view('other.about');
+})->name('other.about');
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('', function () {
+        return view('admin.index');
+    })->name('admin.index');
+    Route::get('create', function () { 
+        return view('admin.create');
+    })->name('admin.create');
+    Route::post('create', function(\Illuminate\Http\Request $request, \Illuminate\Validation\Factory $validator) {
+        $validation = $validator->make($request->all(),[
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+        if($validation->fails())
+            return redirect()->back()->withErrors($validation);
+      return redirect()
+        ->route('admin.index')
+        ->with('info','Post created, Title ' . $request->input('title'));
+    })->name('admin.create');
+    Route::get('edit/{id}', function ($id) {
+           if($id == 1)
+            {
+                $post = [
+                        'title' => 'Learning Laravel',
+                        'content' => 'This blog post will get you right on track with Laravel'
+                ];
+            }else{
+                $post = [
+                                'title' => 'Something else',
+                                'content' => 'Some other content'
+                        ];
+            }
+        return view('admin.edit', ['post' => $post]);
+    })->name('admin.edit');
+    Route::post('edit', function(\Illuminate\Http\Request $request, \Illuminate\Validation\Factory $validator) {
+        $validation = $validator->make($request->all(),[
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+        if($validation->fails())
+            return redirect()->back()->withErrors($validation);
+      return redirect()
+        ->route('admin.index')
+        ->with('info','Post editted, new Title ' . $request->input('title'));
+    })->name('admin.update');
+});
+*/
+/*
+Route::get('/', function () {
+    return view('blog.index');
+})->name('blog.index');
+Route::get('post/{id}', function () {
+    return view('blog.post');
+})->name('blog.post');
+Route::get('about', function () {
+    return view('other.about');
+})->name('other.about');
 Route::get('admin/create', function () {
     return view('admin.create');
 })->name('admin.create');
-
 Route::post('admin/create', function(){
-	return "It works!";
+    return "It works!";
 })->name('admin.create');
-
 Route::get('admin', function () {
     return view('admin.index');
 })->name('admin.index');
-
 Route::get('admin/edit/{id}', function () {
     return view('admin.edit');
 })->name('admin.edit');
-
 Route::post('admin/update', function () {
     return view('admin.update');
-})->name('admin.update');*/
+})->name('admin.update');
+*/

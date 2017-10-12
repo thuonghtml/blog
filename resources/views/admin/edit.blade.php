3db87feed622a0e91f8eb3cc19a42a98e1e88cc3
DESKTOP-U1GLCMN\NguyenMinhThuong <thuonghtml@gmail.com>
2017-10-12 10:20:31 +0700

    Add some methods for PostModel, PostController, Validation in Control…

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -25,6 +25,7 @@
 
                 </div>
                 {{ csrf_field() }}
+                <input type="hidden" name="id" value="{{ $postId }}">
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>

c2e17128cff8cb974444c78a4912466e8fb4694c
DESKTOP-U1GLCMN\NguyenMinhThuong <thuonghtml@gmail.com>
2017-10-12 16:31:03 +0700

    Defining one-to-many relationship

--- app/Like.php
+++ app/Like.php
@@ -3,5 +3,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
-    //
+    public function post()
+    {
+    	return $this->belongsTo('App\Like', 'post_id');
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -4,5 +4,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
 	protected $fillable = ['title', 'content']; #same as constructor
-	
+	public function likes()
+	{
+		return $this->hasMany('App\Like','post_id');
+	}
 }
